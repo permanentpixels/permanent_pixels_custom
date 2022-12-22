6f69e714b476bf746ef6a84efee8e5945f55edc4
Nico Figueroa <nfigue@umich.edu>
2022-12-22 18:41:55 -0500

    Show real navbar on site

---
+++ dist/nav-bar.js
@@ -0,0 +1,53 @@
+"use strict";
+
+function NavBar() {
+  const pages = ["about.html", "gallery.html", "mint.html", "resources.html"];
+  const pageLinks = pages.map(page => {
+    const name = page.substring(0, 1).toUpperCase() + page.substring(1, page.lastIndexOf("."));
+    const currentPage = window.location.pathname;
+    const onPage = currentPage.includes(page);
+    console.log(currentPage.includes(page));
+    return /*#__PURE__*/React.createElement("li", {
+      className: "nav-item px-3"
+    }, onPage ? /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      "aria-current": "page",
+      href: page
+    }, /*#__PURE__*/React.createElement("u", {
+      className: "white"
+    }, /*#__PURE__*/React.createElement("h5", null, name))) : /*#__PURE__*/React.createElement("a", {
+      className: "nav-link",
+      href: page
+    }, /*#__PURE__*/React.createElement("h5", null, name)));
+  });
+  return /*#__PURE__*/React.createElement("nav", {
+    className: "navbar navbar-expand-lg bg-black px-6"
+  }, /*#__PURE__*/React.createElement("div", {
+    className: "container-fluid"
+  }, /*#__PURE__*/React.createElement("a", {
+    href: "./index.html",
+    className: "navbar-brand"
+  }, /*#__PURE__*/React.createElement("img", {
+    src: "./images/logo.svg",
+    alt: "Permanent Pixels Logo",
+    width: "50",
+    height: "50"
+  })), /*#__PURE__*/React.createElement("button", {
+    className: "navbar-toggler custom-toggler border-0",
+    type: "button",
+    "data-bs-toggle": "collapse",
+    "data-bs-target": "#navbarSupportedContent",
+    "aria-controls": "navbarSupportedContent",
+    "aria-expanded": "false",
+    "aria-label": "Toggle navigation"
+  }, /*#__PURE__*/React.createElement("span", {
+    className: "navbar-toggler-icon"
+  }))), /*#__PURE__*/React.createElement("div", {
+    className: "collapse navbar-collapse",
+    id: "navbarSupportedContent"
+  }, /*#__PURE__*/React.createElement("ul", {
+    className: "navbar-nav me-auto mb-lg-0 text-center"
+  }, pageLinks)));
+}
+
+ReactDOM.createRoot(document.querySelector("#nav")).render(React.createElement(NavBar));
